ddeb30268f20a7b9243e8bc8ecf1539a26bc2697
Alexandru Al-Qassam <adde.alqassam@gmail.com>
2025-09-02 14:09:18 +0200

    Räknat ut och testat variabler och matematik

--- src/Exercize1_2.java
+++ src/Exercize1_2.java
@@ -1,2 +1,16 @@
 public class Exercize1_2 {
+    public static void main(String[] args) {
+        double length = 2.6; // längd i meter
+        double width = 3.4; //Bredd i meter
+
+        double area = length * width;
+        double perimeter = 2 * (length + width );
+
+        System.out.println("Längd: " + length + "meter");
+        System.out.println("Bredd: " + width + "meter");
+        System.out.println("Arean är: " + area + " kvadratmeter");
+        System.out.println("Omkretsen är: " + perimeter + " meter");
+
+    }
+
 }
