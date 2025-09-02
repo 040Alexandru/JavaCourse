19a8b7f993fd48d6e91ddf129e26cb7cf4cb35b9
Alexandru Al-Qassam <adde.alqassam@gmail.com>
2025-09-02 10:55:55 +0200

    Exercise1_1

---
+++ src/Exercise1_1.java
@@ -0,0 +1,10 @@
+public class Exercise1_1 {
+    public static void main(String[] args) {
+        System.out.println("Alexandru Al-Qassam");
+        int age = 36;
+        System.out.println("Ålder: " + age);
+        System.out.println("Studerar: Systemutveckling med testinriktning");
+        System.out.println("Jag vill saddla om till en framtidssäkrad bransch");
+    }
+
+}
