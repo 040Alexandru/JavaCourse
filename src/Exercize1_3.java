a404be6563a5fe0710109c841ed6dc2389d1a5c1
Alexandru Al-Qassam <adde.alqassam@gmail.com>
2025-09-02 19:58:05 +0200

    Genomfört övning och använt olika variabler som string, inte, double och boolean.

---
+++ src/Exercize1_3.java
@@ -0,0 +1,20 @@
+public class Exercize1_3 {
+    public static void main(String[] args) {
+        String name = "Alexandru";
+        String favoriteFood = "Burgers";
+        int age = 36;
+        int numSiblings = 2;
+        double length = 1.80;
+        boolean likeCoffee = false;
+        System.out.println("Hey, My name is " + name + ".");
+        System.out.println("My favorite food is " + favoriteFood + ".");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I have " + numSiblings + " siblings.");
+        System.out.println("I am " + length + " meters tall.");
+        if (likeCoffee) {
+            System.out.println("I like coffee.");
+        } else {
+            System.out.println("I don't like coffee.");
+        }
+    }
+}
