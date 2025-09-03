be316bc4dad759fddf33b65cccd81f4ff3769430
Alexandru Al-Qassam <adde.alqassam@gmail.com>
2025-09-03 16:17:05 +0200

    La till program som frågar med if och else-satser

---
+++ src/Exercise3_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hur gammal är du?");
+        int age = input.nextInt();
+        if (age > 18) {
+            System.out.println("Grattis! Du får köra bil.");
+        } else {
+            System.out.println("Tyvärr! Du får inte köra bil!");
+        }
+        if (age > 20){
+            System.out.println("Grattis! Du får både köpa alkohol och handla på Systembolaget!");
+        } else {
+            System.out.println("Tyvärr får du varken handla alkohol eller på Systembolaget.");
+        }
+        if (age >65){
+            System.out.println("Dessvärre är du också en pensionär.");
+        } else {
+            System.out.println("Men än är du inte pensionär!");
+        }
+        }
+    }
