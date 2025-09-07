298549727254fbe7361d9f896feae09cde32c67b
Alexandru Al-Qassam <adde.alqassam@gmail.com>
2025-09-07 20:34:10 +0200

    Priskalkylator – KLAR

--- src/Exercise4_2.java
+++ src/Exercise4_2.java
@@ -3,12 +3,32 @@ public class Exercise4_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Vad kostar varan?");
-        double vara = input.nextDouble();
+        double pris = input.nextDouble();
+        input.nextLine();
+
         System.out.println("Är du student?");
-        boolean student = true;
         String studentSvar = input.nextLine();
+
         System.out.println("Är du medlem?");
-        boolean medlem = true;
         String medlemSvar = input.nextLine();
-        }
+
+        boolean isStudent = studentSvar.equalsIgnoreCase("ja");
+        boolean isMedlem = medlemSvar.equalsIgnoreCase("ja");
+
+        double rabattProcent = 0;
+        if (isStudent && isMedlem){
+            rabattProcent = 15;
+        } else if (isStudent){
+            rabattProcent = 10;
+        } else if (isMedlem){
+            rabattProcent = 5;
+        } else rabattProcent = 0;
+
+        double rabattBelopp = pris * rabattProcent / 100.0;
+        double slutPris = pris - rabattBelopp;
+
+        System.out.println("Originalpris: " + (int) pris + " kr");
+        System.out.println("Rabatt: " + (int) rabattBelopp + " kr");
+        System.out.println("Slutpris: " + (int) slutPris + " kr");
+    }
 }
